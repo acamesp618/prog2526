feafe995b0890743e2a582eeda8e1ef66a44ed85
Adrian Campos <acamesp618@g.educaand.es>
2025-09-30 13:49:21 +0200

    Primer commit

---
+++ comparacion_numeros.py
@@ -0,0 +1,10 @@
+numero1 = int(input ("Escriba un numero:"))
+numero2 = int (input ("Escriba otro número:"))
+if numero1<numero2:
+    print ("El numero", numero1, "es menor que", numero2)
+#calcular si los numeros son iguales
+elif numero1==numero2:
+    print ("Los numeros son iguales")
+else:
+    print ("El numero", numero1, "es mayor que", numero2)
+
