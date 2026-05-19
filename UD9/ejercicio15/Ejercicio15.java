75a6d1ecd471edd5b2bab83b3b68ea85f32841fc
Adrian Campos <acamesp618@g.educaand.es>
2026-05-19 14:21:46 +0200

    Add files via upload

--- UD9/ejercicio15/Ejercicio15.java
+++ UD9/ejercicio15/Ejercicio15.java
@@ -12,42 +12,53 @@ public class Ejercicio15 {
 
 	public static void main(String[] args) {
 		try {
-            //TODO: 1. Crear la factoría y el constructor de documentos
+            // 1. Crear la factoría y el constructor de documentos
            File file = new File("config.xml");
            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
            DocumentBuilder builder = factory.newDocumentBuilder();
            
-            //TODO: 2. Parsear el archivo XML
+            // 2. Parsear el archivo XML
            Document documento = builder.parse(file);
            System.out.println("Documento parseado!");
 
             // Opcional pero recomendado: Normaliza el árbol XML
             documento.getDocumentElement().normalize();
 
-            //TODO: 3. Obtener el elemento raíz (<configuracion>)
+            // 3. Obtener el elemento raíz (<configuracion>)
             Element raiz = documento.getDocumentElement();
             
             System.out.println("Elemento raíz: " + raiz.getNodeName());
             
-            //TODO: 4. Obtener los hijos de <base-datos>
-            NodeList listaBaseDatos = documento.getElementsByTagName("base-datos");
-
+            // 4. Obtener los hijos de <base-datos>
+            NodeList listaBaseDatos = raiz.getElementsByTagName("base-datos");
+            
             // Tomamos el primero
             Node nodoBaseDatos = listaBaseDatos.item(0);
+            System.out.println(nodoBaseDatos.getChildNodes());
             
-            //TODO: 5. Vamos a ver sus hijos directos
+            // 5. Vamos a ver sus hijos directos
             NodeList hijos = nodoBaseDatos.getChildNodes();
 
             System.out.println("\nNúmero de hijos detectados: " + hijos.getLength());
             
             System.out.println("--- Listando hijos ---");
            
-            //TODO: Mostrar todos los nodos
+            // Mostrar todos los nodos
             for (int i = 0; i < hijos.getLength(); i++) {
 
                 Node hijo = hijos.item(i);
+                
+                // Solo elementos reales
+                if (hijo.getNodeType() == Node.ELEMENT_NODE) {
 
-                System.out.println("Hijo " + i + " -> Tipo: " + hijo.getNodeType() + " | Nombre: " + hijo.getNodeName());
+                    Element elemento = (Element) hijo;
+
+                    System.out.println(
+                            "Hijo " + i
+                            + " -> Tipo: " + hijo.getNodeType()
+                            + " | Nombre: " + hijo.getNodeName()
+                    );
+                }
             }
 
         } catch (Exception e) {
